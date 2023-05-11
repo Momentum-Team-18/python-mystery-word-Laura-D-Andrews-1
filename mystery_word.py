4cd1bd74d0ca88072e2892c11a5fdf7c0d12f7ea
Laura-D-Andrews <ldandrews88@gmail.com>
2023-05-10 20:34:45 -0400

    created dictionary

--- mystery_word.py
+++ mystery_word.py
@@ -7,25 +7,40 @@ def play_game(filename):
     print("Mission: Protect the city from a scary dragon by correctly guessing each letter in the mystery word")
     print("Rules:")
     print('1. You have 8 lives \u2665')
+
     with open(filename) as file:
         word_list = file.read().split()
         word_random = random.choice(word_list)
     print(word_random)
+
     display = ''
     for letter in word_random:
         display += ' _'
     print("The mystery word is: " + display)
-    # now list looks like []
+
+    display_nested_list = []
+    display_nested_list.append([*display])  # list looks like [[]]
+    display_letter_list = sum(display_nested_list, [])
+    del display_letter_list[::2]
+    print(display_letter_list)
+
     nested_letter_list = []
-    nested_letter_list.append([*word_random])  # list looks like [[]]
-    letter_list = sum(nested_letter_list, [])
-    print(letter_list)
+    nested_letter_list.append([*word_random])
+    letter_list_key = sum(nested_letter_list, [])
+
+    letter_list_value = letter_list_key.copy()
+    letters_dictionary = dict(zip(letter_list_key, letter_list_value))
+    print(letters_dictionary)
+
+    display_dictionary
 
     lives = 3
     while lives > 0:
         letter = input("guess a letter: ")
         if letter in word_random:
             print('Correct')
+            print(display)
+            print(letters_dictionary[letter])
 
         else:
             lives -= 1
