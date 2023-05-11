633bb9ac33abea6df6538fb9263e1218192a9b22
Laura-D-Andrews <ldandrews88@gmail.com>
2023-05-10 20:07:37 -0400

    initial commit

--- mystery_word.py
+++ mystery_word.py
@@ -1,6 +1,39 @@
-def play_game():
-    pass
+
+import random
+
+
+def play_game(filename):
+    print("Guess The Mystery Word")
+    print("Mission: Protect the city from a scary dragon by correctly guessing each letter in the mystery word")
+    print("Rules:")
+    print('1. You have 8 lives \u2665')
+    with open(filename) as file:
+        word_list = file.read().split()
+        word_random = random.choice(word_list)
+    print(word_random)
+    display = ''
+    for letter in word_random:
+        display += ' _'
+    print("The mystery word is: " + display)
+    # now list looks like []
+    nested_letter_list = []
+    nested_letter_list.append([*word_random])  # list looks like [[]]
+    letter_list = sum(nested_letter_list, [])
+    print(letter_list)
+
+    lives = 3
+    while lives > 0:
+        letter = input("guess a letter: ")
+        if letter in word_random:
+            print('Correct')
+
+        else:
+            lives -= 1
+            print("Incorrect. You have " +
+                  str(lives) + ' ' + "lives " + "\u2665 "  "left.")
+
+    print("Game Over. The scary dragon wins")
 
 
 if __name__ == "__main__":
-    play_game()
+    play_game("words.txt")
